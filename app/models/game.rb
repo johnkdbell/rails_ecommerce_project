a686e1509fe6109bd6e4b45719b521d926417591
johnkdbell <johnkbell@hotmail.com>
2020-11-18 20:22:32 -0600

    Correct game and platform models to associate

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,8 @@
 class Game < ApplicationRecord
   has_one_attached :image
+
   belongs_to :platform
+
   validates :name, presence: true, uniqueness: true
   validates :release_date, :genre, :developer, presence: true
   validates :price, presence: true, numericality: true
