b3a5c19da9828bfd189cdedc4d9461ae5ecdad97
johnkdbell <johnkbell@hotmail.com>
2020-11-19 01:41:58 -0600

    Games are displayed

--- app/admin/games.rb
+++ app/admin/games.rb
@@ -7,7 +7,7 @@ ActiveAdmin.register Game do
     f.inputs
     f.inputs do
       f.input :release_date, as: :date_picker
-      f.input :cover_art, as: :file, hint: f.object.cover_art.present? ? image_tag(f.object.image.variant(resize_to_limit: [600, 600])) : ""
+      f.input :image, as: :file, hint: f.object.image.present? ? image_tag(f.object.image.variant(resize_to_limit: [600, 600])) : ""
     end
     f.actions         # adds the 'Submit' and 'Cancel' buttons
   end
--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,9 @@
 class GamesController < ApplicationController
   def index
+    @games = Game.includes(:platform).order("name ASC")
   end
 
   def show
+    @game = Game.find(params[:id])
   end
 end
